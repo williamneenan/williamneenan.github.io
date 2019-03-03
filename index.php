8cf49f5c46c40382bad2736eca01b4f3d2f9f7b8
JB Maddox <aikidesign@gmail.com>
2019-03-03 18:23:10 -0500

    initial copy from prev site

---
+++ index.php
@@ -0,0 +1,88 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+<title>welcome | williamneenan.com</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="welcome | williamneenan.com" />
+    <meta name="description" content="William Neenan Acting CV/Resume" />
+    <meta name="keywords" content="william neenan, bill neenan, actor, british actor, voiceover, William Neenan, Bill Neenan, Eccentric Dancing" />
+    <meta name="language" content="EN-US" />
+    <meta name="author" content="jb maddox" />
+    <meta name="copyright" content="1995-<?php echo date("Y"); ?> william neenan" />
+    <meta name="robots" content="INDEX, FOLLOW" />
+    <meta name="revisit-after" content="7 days" />
+    <meta name="reply-to" content="bill@williamneenan.com" />
+    <meta name="document-class" content="Living Document" />
+    <meta name="document-rights" content="Copyrighted Work" />
+    <meta name="document-type" content="Web Page" />
+
+    <meta name="document-rating" content="General" />
+    <meta name="document-distribution" content="Global" />
+    <meta name="document-state" content="Static" />
+    <meta name="cache-control" content="Public" />
+    <meta http-equiv="Content-Language" content="EN-US" />
+
+        <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/styles.css" rel="stylesheet">
+
+</head>
+
+  <body>
+
+    <div class="container">
+
+<div class="jumbotron">
+      <div class="row ">
+        <div id="leftcolumn" class="col-md-6">
+          <h3>William Neenan</h3>
+          <br/>
+          <p class="basic"><strong>Alexander White Agency</strong> <br/>
+          Tel: 323.577.3811 / 404.324.6164<br>
+          <a href="mailto:info@alexanderwhiteagency.com" >info@alexanderwhiteagency.com</a>
+          </p>
+
+          <p><a href="http://www.imdb.com/name/nm0624242/" target="_blank">IMDB</a> | <a href="resume/william_neenan_resume_pdf.pdf">CV/R&eacute;sum&eacute;</a>
+            <br /><a href="/files/William-Neenan-Commercial.mp3" target="_blank">Commercial Audio Reel</a><br /><a href="/files/William-Neenan-Documentary-and-Narrative.mp3" target="_blank">Documentary/Narrative Audio Reel</a></p>
+        </div>
+
+        <div id="rightcolumn" class="col-md-6">
+          <a href="resume/william_neenan_resume_pdf.pdf"><img src="images/bill_neenan_photo_thumb.jpg" alt="Hi, I'm Bill..." class="img-thumbnail"></a>
+        </div>
+      </div>
+</div>
+
+
+
+      <div class="jumbotron">
+        <h4>Ride Along</h4>
+        <iframe width="580" height="346" src="//www.youtube.com/embed/ZVInVqcijUM" frameborder="0" allowfullscreen></iframe>
+        <h4>Show Reel</h4>
+        <iframe width="580" height="346" src="//www.youtube.com/embed/IvjFw7k5bXk" frameborder="0" allowfullscreen></iframe>
+        <h4>Aria clip</h4>
+        <iframe width="580" height="326" src="//www.youtube.com/embed/UgsTl3htomM" frameborder="0" allowfullscreen></iframe>
+        <h4>Catania! clip</h4>
+        <iframe width="580" height="346" src="//www.youtube.com/embed/Vy58486YMfA" frameborder="0" allowfullscreen></iframe>
+      </div>
+
+
+
+
+      <div class="footer">
+        <p>&copy; William Neenan <?php echo date('Y'); ?></p>
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+</html>
